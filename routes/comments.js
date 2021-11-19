5b7da27b992cc6b00f5c9c7d198f0facfbce720a
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-11-18 19:23:10 -0700

    Add put and delete methods for replies

--- routes/comments.js
+++ routes/comments.js
@@ -101,41 +101,39 @@ router.post("/:id/replies", async (req, res) => {
   }
 });
 
-// router.put("/:id", async (req, res) => {
-//   try {
-//     const { error } = validateComment(req.body);
-//     if (error) return res.status(400).send(error);
-//     const comment = await Comment.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//       videoId: req.body.videoId,
-//       text: req.body.text,
-//       likes: req.body.likes,
-//       dislikes: req.body.dislikes,
-//       replies: req.body.replies
-//       },
-//       { new: true }
-//     );
-//     if (!comment)
-//       return res.status(400).send(`The comment with id "${req.params.id}" d
-//    oes not exist.`);
-//     await comment.save();
-//     return res.send(comment);
-//   } catch (ex) {
-//     return res.status(500).send(`Internal Server Error: ${ex}`);
-//   }
-// });
+router.put("/:id/replies/:id", async (req, res) => {
+  try {
+    const { error } = validateReply(req.body);
+    if (error) return res.status(400).send(error);
+    const reply = await Reply.findByIdAndUpdate(
+      req.params.id,
+      {
+      text: req.body.text,
+      likes: req.body.likes,
+      dislikes: req.body.dislikes,
+      },
+      { new: true }
+    );
+    if (!reply)
+      return res.status(400).send(`The reply with id "${req.params.id}" d
+   oes not exist.`);
+    await reply.save();
+    return res.send(reply);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
 
-// router.delete("/:id", async (req, res) => {
-//   try {
-//     const comment = await Comment.findByIdAndRemove(req.params.id);
-//     if (!comment)
-//       return res.status(400).send(`The product with id "${req.params.id}" d
-//    oes not exist.`);
-//     return res.send(comment);
-//   } catch (ex) {
-//     return res.status(500).send(`Internal Server Error: ${ex}`);
-//   }
-// });
+router.delete("/:id/replies/:id", async (req, res) => {
+  try {
+    const reply = await Reply.findByIdAndRemove(req.params.id);
+    if (!reply)
+      return res.status(400).send(`The reply with id "${req.params.id}" d
+   oes not exist.`);
+    return res.send(reply);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
 
 module.exports = router;
