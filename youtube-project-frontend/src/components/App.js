d0a10460ed670eed186088230010690b2fbff7cb
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-11-22 20:07:01 -0700

    Map comments and replies

--- youtube-project-frontend/src/components/App.js
+++ youtube-project-frontend/src/components/App.js
@@ -8,25 +8,37 @@ const App = () => {
   const [comments, setComments] = useState([]);
 
   const getVideo = async () => {
-    await axios.get(
-      `https://www.googleapis.com/youtube/v3/search?q=dogs&key=${API_KEY}&part=snippet`
-    ).then(res =>setVideoImage(res.data.items[0].snippet.thumbnails.medium.url))
+    await axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/search?q=dogs&key=${API_KEY}&part=snippet`
+      )
+      .then((res) =>
+        setVideoImage(res.data.items[0].snippet.thumbnails.medium.url)
+      );
   };
-
 
   const getComments = async () => {
-    await axios.get(
-      `http://localhost:8000/api/comments`
-    ).then(res =>setComments(res.data))
+    await axios
+      .get(`http://localhost:8000/api/comments`)
+      .then((res) => setComments(res.data));
   };
-
- 
 
   return (
     <div>
-        <img src={videoImage} height="180" width="320"/>
+      {/* <img src={comments} height="180" width="320"/> */}
       <button onClick={() => getComments()}>Get Comments</button>
-      {key}
+      <ul>
+        {comments.map((comment, i) => (
+          <li key={i}>
+            {comment.text}
+            <ul>
+              {comment.replies.map((reply, i) => (
+                <li key={i}>{reply.text}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
