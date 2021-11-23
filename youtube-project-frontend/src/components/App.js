fa2f0212e4fc6c4ae0d609d4b924777f8b9dfeee
Ryan Sullivan <ryan.sullivan0319@gmail.com>
2021-11-22 17:47:31 -0700

    Connected front end to server get all + installed CORS

--- youtube-project-frontend/src/components/App.js
+++ youtube-project-frontend/src/components/App.js
@@ -5,6 +5,7 @@ import API_KEY from "../YT_API_KEY/API_KEY";
 const App = () => {
   const [key, setKey] = useState(API_KEY);
   const [videoImage, setVideoImage] = useState();
+  const [comments, setComments] = useState([]);
 
   const getVideo = async () => {
     await axios.get(
@@ -12,10 +13,19 @@ const App = () => {
     ).then(res =>setVideoImage(res.data.items[0].snippet.thumbnails.medium.url))
   };
 
+
+  const getComments = async () => {
+    await axios.get(
+      `http://localhost:8000/api/comments`
+    ).then(res =>setComments(res.data))
+  };
+
+ 
+
   return (
     <div>
         <img src={videoImage} height="180" width="320"/>
-      <button onClick={() => getVideo()}>Get Img</button>
+      <button onClick={() => getComments()}>Get Comments</button>
       {key}
     </div>
   );
--- youtube-project/index.js
+++ youtube-project/index.js
@@ -2,10 +2,12 @@ const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
 const comments = require('./routes/comments');
+const cors = require('cors');
 
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/comments", comments);
 // app.use("/api/replies", replies);
